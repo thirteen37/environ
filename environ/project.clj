fad7fd34ed1b7137a3748eab1cb4fdf139f6854b
Yu-Xi Lim <limyuxi@gmail.com>
2013-03-17 00:08:33 +0800

    Update project.clj for Clojars

--- environ/project.clj
+++ environ/project.clj
@@ -1,6 +1,6 @@
-(defproject environ "0.4.0"
+(defproject org.clojars.thirteen37/environ "0.4.0"
   :description "Core environ library for use in applications"
-  :url "https://github.com/weavejester/environ"
+  :url "https://github.com/thirteen37/environ"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]])
